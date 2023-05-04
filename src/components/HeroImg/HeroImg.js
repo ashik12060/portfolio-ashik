6377248509a844f55db44ae6fd029e3ba81aac68
Ashik Mahmood <wd.ashik04@gmail.com>
2023-05-04 11:11:48 +0600

    button restored

--- src/components/HeroImg/HeroImg.js
+++ src/components/HeroImg/HeroImg.js
@@ -10,18 +10,15 @@ const HeroImg = () => {
                 <img className="into-img" src={heroImg} alt="heroImg" />
             </div>
             <div className="content">
-                <p>I'm Ashik Mahmood!</p>
-                <h1 className="header-style">Web Developer</h1>
+                <p className="text-al">I'm Ashik Mahmood!</p>
+                <h1 className="header-style ">Web Developer</h1>
                 <div className="btn-style">
                     <div className=" btn-container-1">
                        
                         <a  className=" btn-btn new-btn-form" href="https://drive.google.com/file/d/1-BQbPRzQAqdGNYv-ScO4C_-sta8pJF8u/view?usp=share_link" target="_blank" rel="noreferrer">Resume</a>
 
                     </div>
-                    <div className="btn-container-1 contact-container">
-                        <Link to="/contact" className=" btn-btn new-btn-form new-link">Contact</Link>
-
-                    </div>
+                    
 
 
                     <div>
